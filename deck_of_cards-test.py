a0207ef8b8dc7eb7fc1c44181be438e097883f95
CorneliaKelinske <corneliakelinske@gmail.com>
2019-07-10 06:29:05 -0600

    first tests added for the Deck class

--- deck_of_cards-test.py
+++ deck_of_cards-test.py
@@ -13,3 +13,17 @@ class CardTests (unittest.TestCase):
     def test_repr(self):
         """repr should return a string saying 'VALUE of SUIT'"""
         self.assertEqual(repr(self.card), "A of Hearts")
+
+
+class DeckTest (unittest.TestCase):
+    def setUp(self):
+        self.deck = Deck()
+    
+    def test_init(self):
+        """Deck should have a cards attribute that is a list of 52 cards"""
+        self.assertTrue(isinstance(self.deck.cards, list))
+        self.assertEqual(len(self.deck.cards), 52)
+    
+    def test_repr(self):
+        """repr should return a string saying how many cards are in the deck"""
+        self.assertEqual(repr(self.deck), "Deck of 52 cards")
