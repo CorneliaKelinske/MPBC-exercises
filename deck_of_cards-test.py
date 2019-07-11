faf03c1ef914f3d7edbf8d005e47f42831790c1e
CorneliaKelinske <corneliakelinske@gmail.com>
2019-07-11 06:39:16 -0600

    more tests added

--- deck_of_cards-test.py
+++ deck_of_cards-test.py
@@ -27,3 +27,15 @@ class DeckTest (unittest.TestCase):
     def test_repr(self):
         """repr should return a string saying how many cards are in the deck"""
         self.assertEqual(repr(self.deck), "Deck of 52 cards")
+    
+    def test_count(self):
+        """count should return the number of cards that are currently in the deck"""
+        self.assertEqual(self.deck.count(), 52)
+        self.deck.cards.pop()
+        self.assertEqual(self.deck.count(), 51)
+
+    def test_deal_sufficient_cards(self):
+        """_deal should return the number of cards specified, and take that numbe of cards away from the deck"""
+        cards = self.deck._deal(10)
+        self.assertEqual(len(cards), 10)
+        self.assertEqual(se;f.deck.count(), 42)
