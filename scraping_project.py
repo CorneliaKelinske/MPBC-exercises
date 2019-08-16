96562c51fa4e0c98a3388899b9c75636ea32ac96
CorneliaKelinske <corneliakelinske@gmail.com>
2019-08-16 06:45:36 -0600

    next step added

--- scraping_project.py
+++ scraping_project.py
@@ -1,6 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
+
+all_quotes = []
 response = requests.get("http://quotes.toscrape.com")
 soup = BeautifulSoup(response.text, "html.parser")
-quotes = soup.find_all("quote")
+quotes = soup.find_all(class_="quote")
+
+for quote in quotes:
+    quote_text = quote.find(class_="text").get_text()
+    print(quote_text)
+
+
+
+
