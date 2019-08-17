304ac06eac4074a7446728191c035a34821fe060
CorneliaKelinske <corneliakelinske@gmail.com>
2019-08-17 07:24:34 -0600

    made it look prettier

--- scraping_project.py
+++ scraping_project.py
@@ -8,10 +8,12 @@ soup = BeautifulSoup(response.text, "html.parser")
 quotes = soup.find_all(class_="quote")
 
 for quote in quotes:
-    a_tag = quote.find("a")
-    all_quotes.append({"text": quote.find(class_="text").get_text(),
-    "author" : quote.find(class_="author").get_text(), 
-    "href": a_tag['href']})
+    
+    all_quotes.append({
+        "text": quote.find(class_="text").get_text(), 
+        "author" : quote.find(class_="author").get_text(), 
+        "bio-link": quote.find("a")["href"]
+        })
 
 print(all_quotes)
 
