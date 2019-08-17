638a77ae2a42c71c541b7104b6182c05c29c4697
CorneliaKelinske <corneliakelinske@gmail.com>
2019-08-17 07:19:25 -0600

    author and href added

--- scraping_project.py
+++ scraping_project.py
@@ -8,9 +8,12 @@ soup = BeautifulSoup(response.text, "html.parser")
 quotes = soup.find_all(class_="quote")
 
 for quote in quotes:
-    quote_text = quote.find(class_="text").get_text()
-    print(quote_text)
+    a_tag = quote.find("a")
+    all_quotes.append({"text": quote.find(class_="text").get_text(),
+    "author" : quote.find(class_="author").get_text(), 
+    "href": a_tag['href']})
 
+print(all_quotes)
 
 
 
