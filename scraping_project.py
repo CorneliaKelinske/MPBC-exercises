c8c150185e71bacc4f21c93b690d18caca82c7fe
CorneliaKelinske <corneliakelinske@gmail.com>
2019-08-22 08:45:50 -0600

    adding on hints

--- scraping_project.py
+++ scraping_project.py
@@ -19,7 +19,7 @@ while url:
                 all_quotes.append({
                         "text": quote.find(class_="text").get_text(), 
                         "author" : quote.find(class_="author").get_text(), 
-                        "bio-link": quote.find("a")["href"]
+                        "bio_link": quote.find("a")["href"]
                         })
         next_button = soup.find(class_="next")
         url = next_button.find("a")["href"] if next_button else None
@@ -36,8 +36,26 @@ guess = " "
 while guess.lower() != chosen_quote["author"].lower() and remaining_guesses > 0:
         guess = input(f"Who said this? Guess! You have {remaining_guesses} more guesses. ")
         remaining_guesses -= 1
+        if remaining_guesses == 3:
+                res = requests.get(f"{base_url}{chosen_quote['bio_link']}")
+                soup = BeautifulSoup(res.text, "html.parser")
+                birth_date = soup.find(class_="author-born-date").get_text()
+                birth_place = soup.find(class_="author-born-location").get_text()
+               
+                print(f"Here is a hint: the author was born on {birth_date} in {birth_place}.")
+                
+        else:
+                print("try again")
+
+
+               
+  
+                #I need to make another scraping call, go to biolink, and then scrape there for author birth date and place
+
        
 print("after while loop")
+
+
 
 
 
