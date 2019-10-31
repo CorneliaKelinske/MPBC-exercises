de75aa9066a38182ed2f1a07c6dd0c72bbf15210
CorneliaKelinske <corneliakelinske@gmail.com>
2019-10-31 06:57:47 -0600

    Bootcamp solution added to find_factors

--- find_factors.py
+++ find_factors.py
@@ -2,3 +2,14 @@ def find_factors(num):
     return [i for i in range(1, num+1) if num%i == 0]
 
 print(find_factors(321421))
+
+
+#The Bootcamp solution:
+def find_factors_BC(num):
+    factors = []
+    i = 1
+    while(i <= num):
+        if (num % i == 0):
+            factors.append(i)
+        i += 1
+    return factors
