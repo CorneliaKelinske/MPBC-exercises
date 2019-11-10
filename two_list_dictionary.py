9c5855b75ae9c6e23555fefa04b227a78d0a632d
CorneliaKelinske <corneliakelinske@gmail.com>
2019-11-09 18:22:09 -0700

    I probably totally butchered the two_list_dictionary exercise, but it works

--- two_list_dictionary.py
+++ two_list_dictionary.py
@@ -1,4 +1,8 @@
 def two_list_dictionary(list_1, list_2):
+    if len(list_1) > len(list_2):
+        list_2.append(None)
+        
+
     return dict(zip(list_1, list_2))
 
 print(two_list_dictionary([1, 2, 3, 4], ["A", "B", "C"]))
