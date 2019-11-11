e73a7165b88efbf995d463225de276ffa1f2e44b
CorneliaKelinske <corneliakelinske@gmail.com>
2019-11-11 07:24:13 -0700

    making progress

--- range_in_list.py
+++ range_in_list.py
@@ -1,4 +1,9 @@
-def range_in_list(list):
-    return sum(list)
+def range_in_list(input_list, start=0, *args):
+    if args:
+        result_list = [item for item in range (input_list[start], input_list[args])]
+    else:
+        result_list = [item for item in range (input_list[start], len(input_list)+1)]
+
+    return result_list
 
 print(range_in_list([1,2,3]))
