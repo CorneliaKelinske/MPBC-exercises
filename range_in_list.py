8ba14d9e6e62e3e771b521b81b345f4748e0f43e
CorneliaKelinske <corneliakelinske@gmail.com>
2019-11-11 08:14:23 -0700

    already curious about the bootcamp solution to the range_in_list exercise

--- range_in_list.py
+++ range_in_list.py
@@ -1,9 +1,16 @@
 def range_in_list(input_list, start=0, *args):
-    if args:
-        result_list = [item for item in range (input_list[start], input_list[args])]
+    if not input_list:
+        return 0
+    elif args and args[0] <= len(input_list):        #end = args[0]
+        result_list = [item for item in range (input_list[start], input_list[args[0]]+1)]
     else:
         result_list = [item for item in range (input_list[start], len(input_list)+1)]
 
-    return result_list
+    return sum(result_list)
 
-print(range_in_list([1,2,3]))
+print(range_in_list([1,2,3,4],0,2)) #  6
+print(range_in_list([1,2,3,4],0,3)) # 10
+print(range_in_list([1,2,3,4],1)) #  9
+print(range_in_list([1,2,3,4])) # 10
+print(range_in_list([1,2,3,4],0,100)) # 10
+print(range_in_list([],0,1)) # 0
