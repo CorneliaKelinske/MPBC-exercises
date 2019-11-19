b4584555702e14640168e938c07559e0e62ac5a7
CorneliaKelinske <corneliakelinske@gmail.com>
2019-11-19 06:44:33 -0700

    another quick exercise

---
+++ nth.py
@@ -0,0 +1,15 @@
+'''
+nth(['a', 'b', 'c', 'd'], 1)  # 'b' 
+nth(['a', 'b', 'c', 'd'], -2) #  'c'
+nth(['a', 'b', 'c', 'd'], 0)  # 'a'
+nth(['a', 'b', 'c', 'd'], -4) #  'a'
+nth(['a', 'b', 'c', 'd'], -1) #  'd'
+nth(['a', 'b', 'c', 'd'], 3)  # 'd'
+'''
+
+def nth(list, num):
+    if num < 0:
+        return list[len(list) + num]
+    return list[num]
+
+print(nth(['a', 'b', 'c', 'd'], -1))
