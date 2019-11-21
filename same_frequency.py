d663259b9312478dd1ea16e5619d1073f90d941c
CorneliaKelinske <corneliakelinske@gmail.com>
2019-11-21 06:04:27 -0700

    added the BC solution to the same_frequency exercise

--- same_frequency.py
+++ same_frequency.py
@@ -8,3 +8,14 @@ def same_frequency(num1, num2):
 
 
 print(same_frequency(321142,3212215))
+
+def same_frequency_BC(num1,num2):
+    d1 = {letter:str(num1).count(letter) for letter in str(num1)}
+    d2 = {letter:str(num2).count(letter) for letter in str(num2)}
+  
+    for key,val in d1.items():
+        if not key in d2.keys():
+            return False
+        elif d2[key] != d1[key]:
+            return False
+    return True
