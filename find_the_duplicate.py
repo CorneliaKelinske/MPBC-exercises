3b00726f7a42cf81cd2732551c0e563973b6935f
CorneliaKelinske <corneliakelinske@gmail.com>
2019-11-21 06:10:47 -0700

    added the BC solution to the find_the_duplicate_exercise

--- find_the_duplicate.py
+++ find_the_duplicate.py
@@ -20,3 +20,14 @@ def find_the_duplicate(input_list):
 print(find_the_duplicate([1,2,1,4,3,12]))
 print(find_the_duplicate([6,1,9,5,3,4,9]))
 print(find_the_duplicate([2,1,3,4]))
+
+def find_the_duplicate_BC(arr):
+    counter = {}
+    for val in arr:
+        if val in counter:
+            counter[val] += 1
+        else:
+            counter[val] = 1
+    for key in counter.keys():
+        if counter[key] > 1:
+            return int(key)
