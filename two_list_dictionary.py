bb08ab5d4692c38c03e85256770d5472d5108e24
CorneliaKelinske <corneliakelinske@gmail.com>
2019-11-21 05:47:48 -0700

    added the BC solution to the two_list_dictionary exercise

--- two_list_dictionary.py
+++ two_list_dictionary.py
@@ -6,3 +6,15 @@ def two_list_dictionary(list_1, list_2):
     return dict(zip(list_1, list_2))
 
 print(two_list_dictionary([1, 2, 3, 4], ["A", "B", "C"]))
+
+
+def two_list_dictionary_BC(keys, values):
+    collection = {}
+ 
+    for idx, val in enumerate(keys):
+        if idx < len(values):
+            collection[keys[idx]] = values[idx]
+        else:
+            collection[keys[idx]] = None
+ 
+    return collection
