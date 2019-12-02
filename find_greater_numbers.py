65f6d9e56c5a75c2d8a1b0ab30e919c63a6daf8f
CorneliaKelinske <corneliakelinske@gmail.com>
2019-12-02 06:45:56 -0700

    don't try this at home. this is not it

--- find_greater_numbers.py
+++ find_greater_numbers.py
@@ -10,15 +10,15 @@ def find_greater_numbers(numbers):
    
     for num in numbers:
         i = 1
-        print("doing the loop")
-        print(f"index is {i}")
-        while i < len(numbers) - 1:
+        #print("doing the loop")
+        #print(f"index is {i}")
+        while numbers.index(num) + i <= len(numbers) - 1:
             if num < numbers[numbers.index(num)+ i]:
                 i +=1
                 count += 1
-                print(f"the count is {count}")
-                print(f"the number is {num}")
+                #print(f"the count is {count}")
+                #print(f"the number is {num}")
 
     return count
 
-print(find_greater_numbers([1,2,3]))
+print(find_greater_numbers([6,1,2,7]))
