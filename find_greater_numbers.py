630959ccbc2f14a9a308ec7a9d2e200d1f589473
CorneliaKelinske <corneliakelinske@gmail.com>
2019-12-02 06:41:15 -0700

    getting started on the find_greater_numbers exercise. need to modify i, so it will only lead me to the end of the list

---
+++ find_greater_numbers.py
@@ -0,0 +1,24 @@
+'''
+find_greater_numbers([1,2,3]) # 3 
+find_greater_numbers([6,1,2,7]) # 4
+find_greater_numbers([5,4,3,2,1]) # 0
+find_greater_numbers([]) # 0
+'''
+
+def find_greater_numbers(numbers):
+    count = 0
+   
+    for num in numbers:
+        i = 1
+        print("doing the loop")
+        print(f"index is {i}")
+        while i < len(numbers) - 1:
+            if num < numbers[numbers.index(num)+ i]:
+                i +=1
+                count += 1
+                print(f"the count is {count}")
+                print(f"the number is {num}")
+
+    return count
+
+print(find_greater_numbers([1,2,3]))
