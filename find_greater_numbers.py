fbafb156aca1b1fd724e9b6b7372761378961514
CorneliaKelinske <corneliakelinske@gmail.com>
2019-12-03 07:20:36 -0700

    trying a different angle

--- find_greater_numbers.py
+++ find_greater_numbers.py
@@ -6,19 +6,7 @@ find_greater_numbers([]) # 0
 '''
 
 def find_greater_numbers(numbers):
-    count = 0
-   
-    for num in numbers:
-        i = 1
-        #print("doing the loop")
-        #print(f"index is {i}")
-        while numbers.index(num) + i <= len(numbers) - 1:
-            if num < numbers[numbers.index(num)+ i]:
-                i +=1
-                count += 1
-                #print(f"the count is {count}")
-                #print(f"the number is {num}")
-
-    return count
-
-print(find_greater_numbers([6,1,2,7]))
+    list_of_numbers = [numbers[numbers.index(num) : len(numbers)] for num in numbers]
+    return(list_of_numbers)
+ 
+print(find_greater_numbers([2,6,5,7]))
