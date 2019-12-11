b26491bd92b3d01b75e2eaf9321e2c34359b979b
CorneliaKelinske <corneliakelinske@gmail.com>
2019-12-11 06:40:20 -0700

    added BC solution to is_odd_string

--- is_odd-string.py
+++ is_odd-string.py
@@ -13,4 +13,8 @@ def is_odd_string(string):
 
 print(is_odd_string('aaaa'))
 
+def is_odd_string_BC(string):
+    total = sum((ord(c) - 96) for c in string.lower()) or 0
+    return total % 2 == 1
+
 
