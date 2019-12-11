7358cf7a1d93bfd7dc985bfe8fa01f48e5a766e3
CorneliaKelinske <corneliakelinske@gmail.com>
2019-12-11 06:32:48 -0700

    added mix_max_key_in_dictionary BC solution

--- min_max_key_in_dictionary.py
+++ min_max_key_in_dictionary.py
@@ -10,3 +10,8 @@ def min_max_key_in_dictionary(dictionary):
     return [min_key, max_key]
  
 print(min_max_key_in_dictionary({1: "Elie", 4:"Matt", 2: "Tim"}))
+
+
+def min_max_key_in_dictionary_PB(d):
+    keys = d.keys()
+    return [min(keys), max(keys)]
