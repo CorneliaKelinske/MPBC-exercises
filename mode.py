2b30d05c44b148b31043a31a48938c7df55293a9
CorneliaKelinske <corneliakelinske@gmail.com>
2019-12-18 07:18:21 -0800

    completed the mode exercise

--- mode.py
+++ mode.py
@@ -1,17 +1,17 @@
 '''
 mode([2,4,1,2,3,3,4,4,5,4,4,6,4,6,7,4]) # 4
 '''
+from collections import Counter
 def mode(numbers):
     count_dict = {}
     
     for num in numbers:
-        if num not in count_dict.keys():
-            print (f"doing the loop for {num}")
+        if num not in count_dict.keys():            
             count = 0
             for num2 in numbers:
                 if num2 == num:
                     count +=1
             count_dict.update({num : count})
-    return count_dict
+    return Counter(count_dict).most_common()[0][0]
     
 print(mode([2,4,1,2,3,3,4,4,5,4,4,6,4,6,7,4]))
