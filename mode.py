83a7a8440590e768e856b6c5a20027982eab420c
CorneliaKelinske <corneliakelinske@gmail.com>
2019-12-17 17:27:16 -0800

    made some progress on the mode exercise

--- mode.py
+++ mode.py
@@ -6,11 +6,12 @@ def mode(numbers):
     
     for num in numbers:
         if num not in count_dict.keys():
+            print (f"doing the loop for {num}")
             count = 0
             for num2 in numbers:
                 if num2 == num:
                     count +=1
-        count_dict.update({num : count})
+            count_dict.update({num : count})
     return count_dict
     
 print(mode([2,4,1,2,3,3,4,4,5,4,4,6,4,6,7,4]))
