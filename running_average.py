b31fd29cd1d485bc2a2d9e847becff74abebf636
CorneliaKelinske <corneliakelinske@gmail.com>
2019-12-20 15:48:03 -0800

    I am getting somewhere with the running_average exercise but have no idea why

--- running_average.py
+++ running_average.py
@@ -9,5 +9,14 @@ rAvg2(1) # 1
 rAvg2(3) # 2
 '''
 
-def running_average():
-    pass
+def running_average(n1):
+    
+    def inner_function(n2):
+        numbers = [n1]
+        numbers.append(n2)
+        print(numbers)
+        return ((n1 + n2) / len(numbers))
+    return inner_function
+
+rAvg = running_average(1)
+print(rAvg(3))
