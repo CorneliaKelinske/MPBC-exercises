f0c06a81d9039d32f96d96e8b0b7824484780b9f
CorneliaKelinske <corneliakelinske@gmail.com>
2019-12-23 13:09:56 -0800

    finished the running_average exercise

--- running_average.py
+++ running_average.py
@@ -9,14 +9,32 @@ rAvg2(1) # 1
 rAvg2(3) # 2
 '''
 
-def running_average(n1):
-    
-    def inner_function(n2):
-        numbers = [n1]
-        numbers.append(n2)
-        print(numbers)
-        return round((n1 + n2) / len(numbers), 2)
-    return inner_function
+# def running_average(n1):
+#     numbers = 0
+#     def inner_function():
+        
+#         numbers.append(n2)
+#         print(numbers)
+#         return round((n1 + n2) / len(numbers), 2)
+#     return inner_function
 
-rAvg = running_average(1)
-print(rAvg(2))
+# rAvg = running_average(1)
+# print(rAvg(2))
+
+def running_average():
+    numbers = []
+    
+    def get_average(num):
+        numbers.append(num)
+        #print(numbers)
+        return round((sum(numbers) / len(numbers)), 2)
+    return get_average
+
+rAvg = running_average()
+print(rAvg(10))
+print(rAvg(11))
+print(rAvg(12))
+
+rAvg2 = running_average()
+print(rAvg2(1)) # 1
+print(rAvg2(3)) # 2
