1541eeaedb07337383d1d7534d435f2d9ef159d3
CorneliaKelinske <corneliakelinske@gmail.com>
2019-12-24 06:33:17 -0800

    added BC solution to reverse_vowels

--- reverse_vowels.py
+++ reverse_vowels.py
@@ -20,3 +20,20 @@ def reverse_vowels(input_str):
 
 
 print(reverse_vowels("why try, shy fly?"))
+
+def reverse_vowels_BC(s):
+    vowels = "aeiou"
+    string = list(s)
+    i, j = 0, len(s) - 1
+    while i < j:
+        if string[i].lower() not in vowels:
+            i += 1
+        elif string[j].lower() not in vowels:
+            j -= 1
+        else:
+            string[i], string[j] = string[j], string[i]
+            i += 1
+            j -= 1
+    return "".join(string)
+
+print(reverse_vowels("why try, shy fly?"))
