e41f7b1d4b5d4f9c5f040e8ba85bccd71493d4b9
CorneliaKelinske <corneliakelinske@gmail.com>
2019-12-24 06:37:15 -0800

    added BC solution to three_ood_numbers

--- three_odd_numbers.py
+++ three_odd_numbers.py
@@ -14,3 +14,18 @@ def three_odd_numbers(numbers):
     return False
 
 print(three_odd_numbers([1,2,3,3,2]))
+
+def three_odd_numbers_BC(arr):
+    i = 0
+    while(i < (len(arr) -2)):
+        total = 0
+        j = i
+        while(j <= i+2):
+            total += arr[j]
+            j+=1
+            
+        if (j-i) % 3 == 0 and total % 2 != 0:
+            return True
+            
+        i+= 1
+    return False
