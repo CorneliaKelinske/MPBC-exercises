273848c0aa2127234b1b29eadaba2d9ca5246247
CorneliaKelinske <corneliakelinske@gmail.com>
2019-12-24 06:22:57 -0800

    added the BC solution to valid parentheses

--- valid_parenthesis.py
+++ valid_parenthesis.py
@@ -17,3 +17,17 @@ def valid_parentheses(parens):
     return True
 
 print(valid_parentheses('())('))
+
+
+def valid_parentheses_BC(parens):
+    count = 0
+    i = 0
+    while i < len(parens):
+        if (parens[i] == '('):
+            count += 1
+        if (parens[i] == ')'):
+            count -= 1
+        if (count < 0):
+            return False
+        i += 1
+    return count == 0
