5d6657632c4dcd9da4f96be2efa0a2544221ba9f
CorneliaKelinske <corneliakelinske@gmail.com>
2020-01-09 12:57:13 -0800

    still working on the once problem; not getting any further

--- once.py
+++ once.py
@@ -10,4 +10,16 @@ oneAddition(12,200) # None
 '''
 
 def once(input_function):
-    
+  
+  def wrapper(*args, **kwargs):
+   
+    return input_function(*args, **kwargs)
+  return wrapper
+  
+
+def add(a, b):
+    return a+b
+
+oneAddition = once(add)
+print(oneAddition(2,2))
+print(oneAddition(2,3))
